c696ccf9a67707ce3b8a7eb41a8098b74ffd808f
Shane Streator <shanestreator@gmail.com>
2019-03-09 00:35:18 -0600

    updated .gitignore

--- client/src/Config/paypal.js
+++
@@ -1,8 +0,0 @@
-// const PAYPAL_PUBLISHABLE =
-//   process.env.NODE_ENV === 'production'
-//     ? 'pk_live_MY_PUBLISHABLE_KEY'
-//     : 'pk_test_ah1LuE7XzmX4SAHAvZg7nw96'
-
-// export default PAYPAL_PUBLISHABLE
-
-// AbOnf4RmtcLWbIxu3BdfTilVSGI9-lGl2Z3dwc91fMvy0RulFBeSTV2sXSsZCDa3Alny5PmHIkIqQ4c8
--- client/src/Config/stripe.js
+++
@@ -1,9 +0,0 @@
-const STRIPE_PUBLISHABLE =
-  process.env.NODE_ENV === 'production'
-    ? 'pk_live_MY_PUBLISHABLE_KEY'
-    : 'pk_test_mF7lWdIizZacpBbEqRzzQB53'
-
-export default STRIPE_PUBLISHABLE
-
-// mark: pk_test_mF7lWdIizZacpBbEqRzzQB53
-// shane: pk_test_ah1LuE7XzmX4SAHAvZg7nw96
