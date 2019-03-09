dd93019d97ebe90c5912f048a00e15ef3ae38ee9
Shane Streator <shanestreator@gmail.com>
2019-03-09 00:18:18 -0600

    updated .gitignore

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-// Determine which enviornment we need to run in
-if (process.env.NODE_ENV === 'production') {
-  // Production
-  module.exports = require('./keys_prod')
-} else {
-  // Development
-  module.exports = require('./keys_dev')
-}
--- config/keys_prod.js
+++
@@ -1,6 +0,0 @@
-// Production
-module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY,
-  FRONTEND_URLS: 'https://www.yourdomain.com'
-}
